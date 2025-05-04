f9b1de44309c6a92fb61a1dfad345e2dc4695647
andremesquitasebrae <113381153+andremesquitasebrae@users.noreply.github.com>
2025-05-04 12:18:29 -0400

    estrutura inicial da aplicação. inclusao de back-end utilizando .net9 , e angular 17.

---
+++ WebApi/Controllers/WelcomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WelcomeController : ControllerBase
+    {
+
+        public WelcomeController()
+        {                
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Olá Mundo!";
+        }
+    }
+}
